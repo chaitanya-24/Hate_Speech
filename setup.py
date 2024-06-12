560de680a9c513ddd0776bb920b1b2ec920dd9ed
Chaitanya Sawant <csawant.cs7@gmail.com>
2024-06-12 14:06:25 +0530

    Added logger and exception

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classifier",
+    version="0.0.1",
+    author="chaitanya-24",
+    author_email="csawant2407@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
